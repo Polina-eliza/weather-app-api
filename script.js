003b4da75a60a358afe9bb0fe111fb35fd21fd6c
Polina <polina.lipse@gmai.com>
2023-01-20 01:06:11 +0400

    added the script for current location identification

--- script.js
+++ script.js
@@ -42,6 +42,25 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 
+// Search location
+
+function searchLocation(position) {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+      position.coords.latitude
+    }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(displayWeatherCondition);
+  }
+
+  function getCurrentLocation(event) {
+      event.preventDefault();
+      navigator.geolocation.getCurrentPosition(searchLocation);
+    }
+
+let currentLocationButton = document.querySelector("#button-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
 
 
 
